03f70c500654564b2d7c6ad490bc0fa7a6f4f4ca
Sajeethan <sajeethan@smartzi.com>
2025-02-21 21:11:58 +0530

    hold server down

--- db.js
+++ db.js
@@ -4,13 +4,21 @@ import dotenv from'dotenv'
 dotenv.config()
 const { Pool } = pg
 
-const poolConfig = {
+let devPoolConfig = {
     user: process.env.PDB_USER,
     password: process.env.PDB_PASSWORD,
     host: process.env.PDB_HOST,
     port: process.env.PDB_PORT,
     database: process.env.PDB_DB_NAME
 }
+
+const poolConfig = process.env.PDB_DB_URL ? {
+    connectionString: process.env.PDB_DB_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  } : devPoolConfig
+
 const pool = new Pool(poolConfig)
 
 export default pool
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -14,20 +14,21 @@ router.post('/insert', async(req, res) => {
         password
     } = req.body
     try {
+        console.log('user registration initiated with payload :',req.body)
         const hashedPassword = await bcrypt.hash(password, 10)
-        // console.log('Sending Query ---> ',`SELECT * FROM pichub_dev_v1.insert_user_details(
-        //     '${username}',
-        //     '${adminList.includes(username)? 1 : 2}',
-        //     '${hashedPassword}',
-        //     '${firstName}',
-        //     '${lastName}',
-        //     '${email}'
-        //  );`)
         const newUser = await pool.query(
             `SELECT * FROM pichub_dev_v1.insert_user_details($1, $2, $3, $4, $5, $6);`,
             [username, adminList.includes(username) ? 1 : 2, hashedPassword, firstName, lastName, email]
         )
-        res.status(201).json({user: newUser})
+        console.log('Query answered')
+        let statusCode = newUser.rows[0]?.code
+        if (statusCode === 1000) {
+            res.status(201).json({user: newUser.rows})
+        } else if ([1001,1002].includes(statusCode)){
+            res.status(409).json({error: newUser.rows.msg})
+        } else {
+            res.status(406).json({error: newUser.rows.msg})
+        }
     } catch (error) {
         res.status(502).json({error: error.message})
     }
