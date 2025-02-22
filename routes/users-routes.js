557ff0f0d4434e7177ca06f0b350bb39351f05ab
Sajeethan <sajeethan@smartzi.com>
2025-02-22 17:21:42 +0530

    timelogger implemented

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
-import { psqlFunctionCaller } from 'spooky-node'
+import { psqlFunctionCaller, timeLogger } from 'spooky-node'
 import databaseConfig from '../configs/database-config.js'
 
 const router = express.Router()
@@ -15,6 +15,7 @@ router.post('/insert', async(req, res) => {
         password
     } = req.body
     try {
+        timeLogger({incident: 'User Registration call'})
         console.log('user registration initiated with payload :',req.body)
         const hashedPassword = await bcrypt.hash(password, 10)
         const newUser = await psqlFunctionCaller({
@@ -23,15 +24,18 @@ router.post('/insert', async(req, res) => {
             schemaName: databaseConfig.schemaName,
             sqlFunctionName: databaseConfig.psqlFunction_signup
         })
-        console.log('Query answered')
+        console.log('Query answered', newUser.rows)
         let statusCode = newUser.rows[0]?.code
         if (statusCode === 2000) {
             res.status(201).json({data: newUser.rows[0]})
         } else {
             res.status(409).json({error: newUser.rows[0]?.msg})
         }
+        timeLogger({incident: 'Query Answered'})
     } catch (error) {
         res.status(502).json({error: error})
+        console.log(error)
+        timeLogger({incident: 'Query failed error'})
     }
 })
 
