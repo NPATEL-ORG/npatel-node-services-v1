05eefd4dc488b54b9cae7419275fcb12862704c5
Sajeethan <sajeethan@smartzi.com>
2025-02-22 14:11:47 +0530

    query services completed by spooky

---
+++ configs/database-config.js
@@ -0,0 +1,15 @@
+import dotenv from'dotenv'
+dotenv.config()
+
+const databaseConfig = {
+    poolConfig: {
+        user: process.env.PDB_USER,
+        password: process.env.PDB_PASSWORD,
+        host: process.env.PDB_HOST,
+        port: process.env.PDB_PORT,
+        database: process.env.PDB_DB_NAME
+    },
+    schemaName: 'pichub_dev_v1'
+}
+
+export default poolConfig
