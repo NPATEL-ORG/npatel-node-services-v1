3681561b7a1e09833629a6ee1e92190d547e514d
Sajeethan <sajeethan@smartzi.com>
2025-02-23 23:45:49 +0530

    folder structure implemented for user functions also

---
+++ configs/admins-config.js
@@ -0,0 +1,4 @@
+export const adminList = [
+    'sajeethan', 
+    'npatel'
+]
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,63 +1,11 @@
 import express from 'express'
-import bcrypt from 'bcrypt'
-import { psqlFunctionCaller, timeLogger } from 'spooky-node'
-import databaseConfig from '../configs/database-config.js'
 import { authenticateToken } from '../middleware/authorization.js'
+import { getUserListController, signupUserController } from '../controllers/user-controller.js'
 
 const router = express.Router()
-const adminList = ['sajeethan', 'npatel']
 
-router.post('/signup', async(req, res) => {
-    const {
-        username,
-        email,
-        firstName,
-        lastName,
-        password
-    } = req.body
-    try {
-        timeLogger({incident: 'User Registration call'})
-        console.log('user registration initiated with payload :',req.body)
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = await psqlFunctionCaller({
-            poolConfig: databaseConfig.poolConfig,
-            params: [username, adminList.includes(username) ? 1 : 2, hashedPassword, firstName, lastName, email],
-            schemaName: databaseConfig.schemaName,
-            sqlFunctionName: databaseConfig.psqlFunction_signup
-        })
-        console.log('Query returns', newUser.rows)
-        let statusCode = newUser.rows[0]?.code
-        if (statusCode === 2000) {
-            res.status(201).json({data: newUser.rows[0]})
-        } else {
-            res.status(409).json({error: newUser.rows[0]?.msg})
-        }
-        timeLogger({incident: 'Query Answered'})
-    } catch (error) {
-        res.status(502).json({error: error})
-        console.log(error)
-        timeLogger({incident: 'Neura returns error'})
-    }
-})
+router.post('/signup', signupUserController )
 
-router.get('/get/list', authenticateToken, async( req, res ) => {
-    try {
-        timeLogger({ incident: 'Userlist requested' })
-        const { limit, offset, searchKey } = req.body
-        const userList = await psqlFunctionCaller({
-            poolConfig: databaseConfig.poolConfig,
-            schemaName: databaseConfig.schemaName,
-            sqlFunctionName: databaseConfig.psqlFunction_getUserList,
-            params: [ searchKey, limit, offset ]
-        })
-        if (userList){
-            res.status(200).json({ userList: userList.rows })
-        }
-    } catch (error) {
-        res.status(502).json({error: error})
-        console.log(error)
-        timeLogger({incident: 'Neura returns error'})
-    }
-})
+router.get('/get/list', authenticateToken, getUserListController )
 
 export default router
