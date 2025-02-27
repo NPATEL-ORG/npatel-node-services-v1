fe1eba71cdff6f3fea5cd69a9396ad48091897d6
Sajeethan <sajeethan@smartzi.com>
2025-02-28 00:11:39 +0530

    auth controller responses generalized

--- configs/response-config.js
+++ configs/response-config.js
@@ -6,5 +6,37 @@ export const responseObject = [
     {
         code: 1101,
         msg: "Incorrect password."
-    }
+    },
+    {
+        code: 1102,
+        msg: "User not verified."
+    },
+    {
+        code: 2103,
+        msg: "Login success."
+    },
+    {
+        code: 1103,
+        msg: "Login fail."
+    },
+    {
+        code: 1500,
+        msg: "Neura returns an error."
+    },
+    {
+        code: 1104,
+        msg: "Token is missing."
+    },
+    {
+        code: 1105,
+        msg: "Jwt refresh token error."
+    },
+    {
+        code: 2101,
+        msg: "Token regenerated."
+    },
+    {
+        code: 2102,
+        msg: "Refresh Token Cleared."
+    },
 ]
