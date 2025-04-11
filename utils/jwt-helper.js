4c39149ccd07421616625d53387692405bdfb080
Ezio <sajeethan@smartzi.com>
2025-04-11 23:04:37 +0530

    auth token time reduced for testing

--- utils/jwt-helper.js
+++ utils/jwt-helper.js
@@ -4,8 +4,8 @@ dotenv.config()
 
 const jwtTokenGenerator = ({ userId, userName, email, firstName, lastName }) => {
     const user = { userId, userName, email, firstName, lastName }
-    const accessToken = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '100m'} )
-    const refreshToken = jwt.sign( user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '150m'} )
+    const accessToken = jwt.sign( user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '30s'} )
+    const refreshToken = jwt.sign( user, process.env.REFRESH_TOKEN_SECRET, {expiresIn: '1m'} )
     return ({accessToken, refreshToken})
 }
 
